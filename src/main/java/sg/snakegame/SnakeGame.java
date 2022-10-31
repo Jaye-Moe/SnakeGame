362c1b22565756490b1c2b577cd3cb8d02922ff3
Josh Moore <joshmoore@gmail.com>
2022-10-31 14:11:05 -0400

    Initial Commit

---
+++ src/main/java/sg/snakegame/SnakeGame.java
@@ -0,0 +1,21 @@
+package sg.snakegame;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SnakeGame extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        System.out.println("Hello World");
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
